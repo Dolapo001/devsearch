b6c8b07350bb0720f284330586b3b2c11d13b2aa
Dolapo24 <adedolapovictoria927@gmail.com>
2023-02-01 13:47:01 +0100

    working on

--- projects/models.py
+++ projects/models.py
@@ -9,7 +9,8 @@ class Project(models.Model):
     demo_link = models.CharField(max_length=2000, null=True, blank=True)
     source_link = models.CharField(max_length=2000, null=True, blank=True)
     tags = models.ManyToManyField('Tag', blank=True)
-    vote_total = models.IntegerField(default=0, null=True,)
+    vote_total = models.IntegerField(default=0, null=True, blank=True)
+    vote_ratio = models.IntegerField(default=0, null=True, blank=True)
     created = models.DateTimeField(auto_now_add=True)
     id = models.UUIDField(default=uuid.uuid4, unique=True,
                           primary_key=True, editable=False)
@@ -23,7 +24,6 @@ class Review(models.Model):
         ('up', 'Up Vote'),
         ('down', 'Down Vote',)
     )
-    # owner =
     project = models.ForeignKey(Project, on_delete=models.CASCADE)
     body = models.TextField(null=True, blank=True)
     value = models.CharField(max_length=200, choices=VOTE_TYPE)
@@ -34,13 +34,15 @@ class Review(models.Model):
     def __str__(self):
         return self.value
 
-    class Tag(models.Model):
-        name = models.CharField(max_length=200)
-        id = models.UUIDField(default=uuid.uuid4, unique=True,
-                              primary_key=True, editable=False)
 
-        def __str__(self):
-            return self.name
+class Tag(models.Model):
+    name = models.CharField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    id = models.UUIDField(default=uuid.uuid4, unique=True,
+                          primary_key=True, editable=False)
+
+    def __str__(self):
+        return self.name
 
 
 
