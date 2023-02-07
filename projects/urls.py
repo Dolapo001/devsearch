e832fe67b0ce7f1e1b006cfd3db74b6bbea6e0ab
Dolapo24 <adedolapovictoria927@gmail.com>
2023-02-07 14:59:58 +0100

    working on

--- projects/urls.py
+++ projects/urls.py
@@ -5,5 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.projects, name="projects"),
     path('project/<str:pk>/', views.project, name="project"),
+
     path('create_project/', views.createProject, name='create_project'),
+    path('update-project/<str:pk>/', views.updateProject, name='update-project'),
 ]
+
