cb044569911c51713b574904ddcc9bac55b69f88
Dolapo24 <adedolapovictoria927@gmail.com>
2023-02-07 13:52:15 +0100

    working on

--- projects/views.py
+++ projects/views.py
@@ -17,5 +17,5 @@ def project(request, pk):
 
 def createProject(request):
     form = ProjectForm()
-    context = {'from': form}
-    return render(request, 'projects/project_form.html', context)
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
