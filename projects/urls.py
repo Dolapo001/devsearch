270790eb0ad6056dc806d54742f673fff665f6a7
Dolapo24 <adedolapovictoria927@gmail.com>
2023-02-07 15:34:58 +0100

    working on

--- projects/urls.py
+++ projects/urls.py
@@ -7,6 +7,12 @@ urlpatterns = [
     path('project/<str:pk>/', views.project, name="project"),
 
     path('create_project/', views.createProject, name='create_project'),
+
     path('update-project/<str:pk>/', views.updateProject, name='update-project'),
+
+    path('delete-project/<str:pk>/', views.deleteProject, name='delete-project'),
+
+
+
 ]
 
