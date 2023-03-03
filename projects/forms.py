764eefce252f938539e36bdec38bcb6cf1ebff40
Dolapo24 <adedolapovictoria927@gmail.com>
2023-03-03 13:47:06 +0100

    working on

--- projects/forms.py
+++ projects/forms.py
@@ -13,5 +13,9 @@ class ProjectForm(ModelForm):
 
     def __init__(self, *args, **kwargs):
         super(ProjectForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'input'})
 
-        self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add tile'})
+
+        #self.fields['title'].widget.attrs.update({'class': 'input', 'placeholder': 'Add tile'})
+        #self.fields['description'].widget.attrs.update({'class': 'input', 'placeholder': 'Add Description'})
