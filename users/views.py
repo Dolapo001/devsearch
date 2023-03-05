a5a395adc83af7336b6e72291829dd596be77380
Dolapo24 <adedolapovictoria927@gmail.com>
2023-03-05 16:33:24 +0100

    working on

--- users/views.py
+++ users/views.py
@@ -7,3 +7,9 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles': profiles}
     return render(request, 'users/profile.html', context)
+
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id=pk)
+    context = {'profile': profile}
+    return render(request, 'users/user-profile.html')
