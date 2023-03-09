0f9d5491284856531842ffa25f7d1c7dbff7e3a7
Dolapo24 <adedolapovictoria927@gmail.com>
2023-03-09 08:40:21 +0100

    working on

--- users/views.py
+++ users/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, authenticate, logout
+from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from .models import Profile
 
@@ -8,9 +9,7 @@ from .models import Profile
 # Create your views here.
 
 
-
 def loginUser(request):
-
     if request.user.is_authenticated:
         return redirect('profiles')
 
@@ -25,8 +24,8 @@ def loginUser(request):
         user = authenticate(request, username=username, password=password)
 
         if user is not None:
-            login(render, user)
-            return redirect('profile')
+            login(request, user)
+            return redirect('profiles')
         else:
             print('Username OR Password is incorrect ')
     return render(request, 'users/login_register.html')
@@ -49,5 +48,6 @@ def userProfile(request, pk):
     topSkills = profile.skill_set.exclude(description__exact="")
     otherSkills = profile.skill_set.filter(description="")
 
-    context = {'profile': profile, 'topskills': topSkills, 'otherSkills': otherSkills}
+    context = {'profile': profile, ' topskills': topSkills, 'otherSkills': otherSkills}
     return render(request, 'users/user-profile.html', context,)
+
